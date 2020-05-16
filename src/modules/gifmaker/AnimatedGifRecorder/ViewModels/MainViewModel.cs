3905f5e47501cd2c60d3fbb4c66bc546ba2ffd5a
Armianto Sumitro <t-arsumi@microsoft.com>
2020-05-16 12:11:45 -0400

    Added states in MainViewModel

--- src/modules/gifmaker/AnimatedGifRecorder/ViewModels/MainViewModel.cs
+++ src/modules/gifmaker/AnimatedGifRecorder/ViewModels/MainViewModel.cs
@@ -1,19 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Navigation;
 using AnimatedGifRecorder.Helpers;
 
 namespace AnimatedGifRecorder.ViewModels
 {
     public class MainViewModel : ObservableObject
     {
-        public MainViewModel()
+        public enum AppState
         {
-
+            None,
+            SelectCaptureRegion,
+            Recording,
+            Paused,
+            Stopping
         }
 
+        public AppState State { get; private set; }
 
+        public MainViewModel()
+        {
+            State = AppState.None;
+        }
+
+        public ICommand RecordPauseButtonCommand 
+        {
+            get
+            {
+                if (recordPauseButtonCommand == null)
+                {
+                    recordPauseButtonCommand = new RelayCommand(param =>
+                    {
+                        if (State == AppState.SelectCaptureRegion || State == AppState.Paused)
+                            State = AppState.Recording;
+                        else if (State == AppState.Paused)
+                            State = AppState.Recording;
+                    });
+                }
+                return recordPauseButtonCommand;
+            }
+        }
+
+        private ICommand recordPauseButtonCommand;
+
+        public ICommand StopButtonCommand
+        {
+            get
+            {
+                if (stopButtonCommand == null)
+                {
+                    stopButtonCommand = new RelayCommand(param =>
+                    {
+                        State = AppState.Stopping;
+                    });
+                }
+                return stopButtonCommand;
+            }
+        }
+
+        private ICommand stopButtonCommand;
     }
 }
